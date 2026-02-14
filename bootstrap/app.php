ac2d29eeb8b6afe71fa50081f46de69d15542210
NiceCode9 <nicecode9@gmail.com>
2026-02-14 18:48:29 +0700

    feat: initialize Laravel application bootstrap with routing, middleware, and exception handling configurations.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            url('payment/callback'),
+            'payment/callback',
         ]);
 
         // Register route middleware aliases
